ad1b35722115ad7d59dec08ab627874a73619864
Diego Fernandes <fernandes.diego_@hotmail.com>
2019-02-17 18:36:41 -0300

    Estado e cidade

--- src/main/java/com/diegof/cursomc/services/CategoriaService.java
+++ src/main/java/com/diegof/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.diegof.cursomc.domain.Categoria;
 import com.diegof.cursomc.repositories.CategoriaRepository;
+import com.diegof.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,10 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id:" + id
+					+ ", Tipo: "+ Categoria.class.getName());
+		}
 		return obj;
 	}
 }
--- src/main/java/com/diegof/cursomc/services/exceptions/ObjectNotFoundException.java
+++ src/main/java/com/diegof/cursomc/services/exceptions/ObjectNotFoundException.java
@@ -1,5 +1,14 @@
 package com.diegof.cursomc.services.exceptions;
 
-public class ObjectNotFoundException {
+public class ObjectNotFoundException extends RuntimeException{
 
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+	public ObjectNotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
 }
