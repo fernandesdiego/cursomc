221cfaec62b58228749b693272c49dafa7d9373c
Diego Fernandes <fernandes.diego_@hotmail.com>
2019-02-17 20:10:14 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/diegof/cursomc/CursomcApplication.java
+++ src/main/java/com/diegof/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.diegof.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,12 @@ import com.diegof.cursomc.domain.Cidade;
 import com.diegof.cursomc.domain.Cliente;
 import com.diegof.cursomc.domain.Endereco;
 import com.diegof.cursomc.domain.Estado;
+import com.diegof.cursomc.domain.Pagamento;
+import com.diegof.cursomc.domain.PagamentoComBoleto;
+import com.diegof.cursomc.domain.PagamentoComCartao;
+import com.diegof.cursomc.domain.Pedido;
 import com.diegof.cursomc.domain.Produto;
+import com.diegof.cursomc.domain.enums.EstadoPagamento;
 import com.diegof.cursomc.domain.enums.TipoCliente;
 import com.diegof.cursomc.repositories.CategoriaRepository;
 import com.diegof.cursomc.repositories.CidadeRepository;
@@ -85,7 +91,18 @@ public class CursomcApplication implements CommandLineRunner{
 		clienteRepository.save(Arrays.asList(cli1));
 		enderecoRepository.save(Arrays.asList(e1,e2));
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
 		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
 		
 		
 	}
--- src/main/java/com/diegof/cursomc/domain/Cliente.java
+++ src/main/java/com/diegof/cursomc/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.diegof.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Cliente implements Serializable{
@@ -28,12 +29,16 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	 
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+@OneToMany(mappedBy="cliente")	
+	private List<Pedido> pedidos = new ArrayList<>(); 
 	
 	public Cliente() {
 		
@@ -103,6 +108,15 @@ public class Cliente implements Serializable{
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
 
 	@Override
 	public int hashCode() {
@@ -128,6 +142,8 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
+
+
 	
 	
 }
